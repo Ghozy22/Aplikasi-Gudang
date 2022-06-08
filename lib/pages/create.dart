1d5ad4bf1e7d92616dac3174c74cba5b7a08ca9f
Ghozy <ghozyarc12@gmail.com>
2022-06-08 19:18:22 +0700

    tampil data dan insert data

--- lib/pages/create.dart
+++ lib/pages/create.dart
@@ -1,16 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:gsheets/gsheets.dart';
 import 'package:project/api/api_sheets_barang.dart';
 import 'package:project/main.dart';
 import 'package:project/models/data.dart';
 import 'package:project/widgets/button.dart';
 import 'package:project/widgets/formWidget.dart';
 
-class create extends StatelessWidget {
+
+class create extends StatefulWidget {
   const create({ Key? key }) : super(key: key);
 
   @override
+  State<create> createState() => _createState();
+}
+
+class _createState extends State<create> {
+
+  List<barang> datas = [];
+
+  @override
+  void didUpdateWidget(covariant create oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+
+    setState(() {
+      
+    });
+  }
+
+  void initState(){
+    super.initState();
+
+    getDatas();
+  }
+
+  Future getDatas() async {
+    final datas = await sheetsBarang.getAll();
+
+    setState(() {
+      this.datas = datas;
+    });
+
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
+  return Scaffold(
       appBar: AppBar(
         title: Text(MyApp.title),
         centerTitle: true,
@@ -18,9 +53,38 @@ class create extends StatelessWidget {
       body: Container(
         alignment: Alignment.center,
         padding: EdgeInsets.all(32),
-        child: FormWidget( onSavedData: (data) async {
-          await sheetsBarang.insert([data.toJson()]);
-        } ),
+        child: Column(
+          children: [
+            FormWidget( 
+              
+              //data: coba(),
+
+              //data: datas.isEmpty ? null : datas[index],
+
+              onSavedData: (data) async {
+              
+
+              final id = await sheetsBarang.getRowCount() + 1;
+
+              final newData = data.copy(id : id);
+
+              await sheetsBarang.insert([newData.toJson()]);
+            },
+            ),
+            SizedBox(height: 20,),
+
+            Column(
+            children: List.generate(datas.length, (index) {
+              return Text(
+                datas[index].sn.toString(),
+                style: const TextStyle(fontSize: 22),
+                
+              );
+            }),
+          ),
+            
+          ],
+        ),
       ),
     );
   }
